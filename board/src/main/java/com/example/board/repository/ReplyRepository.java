57e2d811d2924714a20dcd5cbafd8b2589580259
유창규 <yhs02252@gmail.com>
2024-11-15 18:18:41 +0900

    board Modifying

--- board/src/main/java/com/example/board/repository/ReplyRepository.java
+++ board/src/main/java/com/example/board/repository/ReplyRepository.java
@@ -10,7 +10,7 @@ import com.example.board.entity.Reply;
 public interface ReplyRepository extends JpaRepository<Reply, Long>, QuerydslPredicateExecutor<Reply> {
 
     // reply 테이블의 board_bno 를 이용해 reply 삭제하는 쿼리 메소드 생성
-    @Modifying
+    @Modifying // 데이터에 변화를 주는 구문을 실행 할 시 필요함
     @Query("DELETE FROM Reply r WHERE r.board.bno = :bno")
     void deleteByBno(Long bno);
 }
